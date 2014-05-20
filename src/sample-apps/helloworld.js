05944b51dae34798ccc802f0ea5b14d67a14737e
Dua, Akshay <akshay.dua@intel.com>
2014-05-20 11:00:56 -0700

    Added License header.

--- src/sample-apps/helloworld.js
+++ src/sample-apps/helloworld.js
@@ -13,16 +13,5 @@
  *
  * Created by adua.
  */
-var edisonLib = require("edisonapi");
-
-var query = new edisonLib.ServiceQuery();
-query.initServiceQueryFromFile("./serviceQueries/temperatureServiceQueryMQTT.json");
-
-edisonLib.discoverServices(query, function (serviceSpec) {
-  "use strict";
-
-  console.log("Found " + serviceSpec.type.name + " service at " +
-    serviceSpec.address + ":" + serviceSpec.port + " on interface " +
-    serviceSpec.networkInterface);
-
-});
+var edison = require('edisonapi');
+console.log(edison.sayhello());
