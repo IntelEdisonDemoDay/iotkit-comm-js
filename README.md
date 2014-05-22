84eb23adb4a2712f0252934cb86654996c0ca5dc
Dua, Akshay <akshay.dua@intel.com>
2014-05-22 10:18:35 -0700

    Add cloud to the documentation. Update front page.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-#About edison#
+The Intel Edison Connectivity Framework (IECF) allows one Edison to discover and communicate with other Edisons.
+Additionally, IECF allows an Edison to conveniently publish its data to the cloud and enables other Edisons to
+subscribe to that data from the cloud. The IECF is a library that comes pre-installed on the Edison board in
+two flavors: C and NodeJS.
+
+This documentation focuses on the NodeJS version of the library. To begin developing Edison applications and plugins
+ please {@tutorial start-here}.
--- src/lib/plugins/iotkit/iotkit-client.js
+++ src/lib/plugins/iotkit/iotkit-client.js
@@ -21,21 +21,21 @@ var mqtt = require('mqtt');
 EdisonIoTKitClient.prototype.interface = "edison-iotkit-client-interface";
 
 /**
- * The internal client object this plugin ({@tutorial plugin}) creates to communicate with the remote client.
+ * The internal client object this plugin ({@tutorial plugin}) creates to communicate with the {@tutorial cloud}.
  * @type {object}
  */
 EdisonIoTKitClient.prototype.client = {};
 
 /**
- * The client-side plugin needs a received message handler to process messages coming from the server
+ * The client-side plugin needs a received message handler to process messages coming from the {@tutorial cloud}
  * ({@tutorial plugin}).
  * @type {function}
  */
 EdisonIoTKitClient.prototype.receivedMsgHandler = null;
 
 /**
- * Create a client that connects to the cloud. The cloud is described just like any other service:
- * by using a service specification ({@tutorial cloud}).
+ * Create a client that connects to the {@tutorial cloud}. The cloud is described just like any other service:
+ * by using a service specification.
  * @param serviceSpec {object} specification for the cloud ({@tutorial service-spec})
  * @constructor
  */
@@ -59,7 +59,7 @@ function EdisonIoTKitClient(serviceSpec) {
 
 /**
  * Register a new sensor with the cloud. Once registered, data can be published to the
- * cloud for this sensor ({@tutorial cloud}).
+ * cloud for this sensor (see {@tutorial cloud}).
  * @param sensorname {string} name of the sensor to register
  * @param type {string} supported types are 'temperature.v1.0', 'humidity.v1.0' ({@tutorial cloud})
  * @param unit {string} not supported yet. This parameter is ignored.
@@ -70,7 +70,7 @@ EdisonIoTKitClient.prototype.registerSensor = function(sensorname, type, unit){
 
 /**
  * Send a message to the cloud broker. Equivalent to publishing to a topic.
- * @param msg {string} Message to send to cloud broker.
+ * @param msg {string} Message to send to cloud broker (see {@tutorial cloud}).
  * @param context {object.<string, string>} context.topic is contains the topic string
  */
 EdisonIoTKitClient.prototype.send = function (msg, context) {
@@ -102,7 +102,7 @@ EdisonIoTKitClient.prototype.setReceivedMessageHandler = function(handler) {
 };
 
 /**
- * close connection. Sends FIN to the cloud.
+ * close connection. Sends FIN to the {@tutorial cloud}.
  */
 EdisonIoTKitClient.prototype.done = function () {
     this.client.end();
