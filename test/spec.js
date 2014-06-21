4293db24b532eb8ad554c03ce45793c834ff1faf
Akshay Dua <akshay.dua@intel.com>
2014-06-21 20:41:22 +0530

    middleware: Revert "Revert "middleware: test infrastructure""
    JIRA: EDISON-956
    
    This reverts commit 409cebaeb8b797f1cd933a98d1822a2832f5b4b2.
    
    Change-Id: I0e5fd5618f04f037c32fef265175e9115a60d58c
    Signed-off-by: Akshay Dua <akshay.dua@intel.com>
    Reviewed-on: http://git-ger-5.devtools.intel.com/gerrit/5980
    Reviewed-by: jenkins_ndg <jenkins_ndg@intel.com>
    Reviewed-by: Chereau, Fabien <fabien.chereau@intel.com>
    Tested-by: Chereau, Fabien <fabien.chereau@intel.com>

---
+++ test/spec.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2014, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ */
+
+/** @module test/spec */
+
+/**
+ * @file Tests if a service specification is correctly validated
+ * @see {@link module:test/spec~wellformed|test well-formed service specification}
+ */
+
+var expect = require('chai').expect;
+var path = require('path');
+
+describe('[spec]', function () {
+
+    /**
+     * @function module:test/spec~wellformed
+     */
+    it("should validate a correct spec without throwing an error", function() {
+      var iecf = require('iecf');
+
+      var validator = new iecf.ServiceSpecValidator();
+      validator.readServiceSpecFromFile(path.join(__dirname, "../example/serviceSpecs/mqtt-mini-broker-spec.json"));
+      var spec = validator.getValidatedSpec();
+
+      expect(spec.name).to.be.a('string');
+    });
+});
